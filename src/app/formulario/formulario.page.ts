fa08ffe32e5c54c17c6ac26889e947e896aea1fc
AleRoman <39933694+AleRoman@users.noreply.github.com>
2019-04-01 23:44:35 -0400

    Florerias ya jalan

--- src/app/formulario/formulario.page.ts
+++ src/app/formulario/formulario.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NavParams, ModalController, LoadingController, NavController, ToastController } from '@ionic/angular';
 import { DatosUsuarioService, DatosEnvio } from '../shared/datos-usuario.service';
 import { ObtenerFloresService } from '../shared/obtener-flores.service';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 
 @Component({
@@ -14,6 +15,22 @@ export class FormularioPage implements OnInit {
   untotal = null;
   productos: any;
   datosEnvio: DatosEnvio;
+  florerias: any[] = [];
+
+  // Ubicación de florerias
+  lat: number;
+  long: number;
+
+  // Ubicación del usuario
+  uLat: number;
+  uLong: number;
+
+  // ID de la florería más cercana
+  floreriaCercanaID: string;
+  floreriaCercanaNombre: any;
+
+  mayor = 0;
+  menor = 10000000000;
   constructor(
     private navParams: NavParams,
     private modalController: ModalController,
@@ -21,7 +38,9 @@ export class FormularioPage implements OnInit {
     private nav: NavController,
     private service: DatosUsuarioService,
     private floresService: ObtenerFloresService,
-    public toastCtrl: ToastController) { }
+    public toastCtrl: ToastController,
+    private geolocation: Geolocation,
+  ) { }
 
   ngOnInit() {
     this.untotal = this.navParams.get('eltotal');
@@ -36,9 +55,26 @@ export class FormularioPage implements OnInit {
       total: this.untotal,
       status: false,
       entregado: false,
-      token: this.floresService.token
+      token: this.floresService.token,
+      floreriaId: ''
     };
     this.presentToast(this.floresService.token);
+
+
+    const t = this.floresService.obtenerFlores2();
+    t.valueChanges().subscribe(data => {
+      data.forEach(item => {
+        this.setArreglo(item);
+      });
+    });
+  }
+
+  setArreglo(data) {
+    this.florerias.push(data);
+    console.log(this.florerias);
+    this.getPosition();
+    this.datosEnvio.floreriaId = this.floreriaCercanaID;
+    console.log(this.datosEnvio.floreriaId);
   }
 
   close() {
@@ -54,7 +90,7 @@ export class FormularioPage implements OnInit {
   }
 
   async enviarDatos() {
-
+    console.log(this.datosEnvio.floreriaId);
     const loading = await this.loadingCrtl.create({
       message: 'Enviando pedido...'
     });
@@ -66,6 +102,51 @@ export class FormularioPage implements OnInit {
       this.nav.navigateBack('/app/tabs/tab1');
       this.floresService.resetCart();
     });
+  }
+
+
+  // Método para obtener la posición del usuario
+  getPosition(): any {
+    const locationOptions = { timeout: 30000, enableHighAccuracy: true };
+    this.geolocation.getCurrentPosition(locationOptions).then(response => {
+      this.uLat = response.coords.latitude;
+      this.uLong = response.coords.longitude;
+      this.obtenerFlorerias(this.uLat, this.uLong);
+    })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  // Obtener florerias para calcular distancias
+  obtenerFlorerias(laU: number, lnU: number) {
+    this.florerias.forEach(item => {
+      this.lat = item.lat;
+      this.long = item.long;
+      for (let i = 0; i < this.florerias.length; i++) {
+        if (this.calcularDistancia(this.lat, laU, this.long, lnU) > this.mayor) {
+          this.mayor = this.calcularDistancia(this.lat, laU, this.long, lnU);
+        }
+        if (this.calcularDistancia(this.lat, laU, this.long, lnU) < this.menor) {
+          this.menor = this.calcularDistancia(this.lat, laU, this.long, lnU);
+          this.floreriaCercanaNombre = item.nombre;
+          this.floreriaCercanaID = item.userId;
+          this.datosEnvio.floreriaId = item.userId;
+        }
+      }
+
+    });
+    console.log(this.floreriaCercanaID);
+  }
+
+  // Calcular distancia entre 2 puntos devolviendo un valor en kilometros
+  calcularDistancia(lat1: number, lat2: number, long1: number, long2: number) {
+    const p = 0.017453292519943295;    // Math.PI / 180
+    const c = Math.cos;
+    const a = 0.5 - c((lat1 - lat2) * p) / 2 + c(lat2 * p) * c((lat1) * p) * (1 - c(((long1 - long2) * p))) / 2;
+    const dis = (12742 * Math.asin(Math.sqrt(a))); // 2 * R; R = 6371 km
+    console.log(dis);
+    return dis;
   }
 }
 
--- src/app/shared/datos-usuario.service.ts
+++ src/app/shared/datos-usuario.service.ts
@@ -14,6 +14,7 @@ export interface DatosEnvio {
   total: number;
   entregado: boolean;
   token: string;
+  floreriaId: string;
 }
 
 @Injectable({
